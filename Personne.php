0f81cfc893a4b9b74eec17acc95420c77a2cc81d
Nahim BENBAHLOULI <benbahlouli.nahim@gmail.com>
2023-04-26 11:42:04 +0200

    modif de la classe personne

--- Personne.php
+++ Personne.php
@@ -1,10 +1,25 @@
 <?php
 class Personne {
-    public $nom;
-    public $prenom;
-    public $age;
-    public $genre;
-    public $isSmoker;
+    private $nom;
+    private $prenom;
+    private $age;
+    private $genre;
+    private $isSmoker;
+
+    public function __construct($nom, $prenom, $age, $genre, $smoker){
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->age = $age;
+        $this->genre = $genre;
+        $this->isSmoker = $smoker;
+    }
+
+    public function Personne($nom, $prenom, $age, $genre){
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->age = $age;
+        $this->genre = $genre;
+    }
 
     public function smoker(){
         if($this->isSmoker){
@@ -14,11 +29,36 @@ class Personne {
             return "Il ne fume pas";
         }
     }
+
+    public function isAdult($majorite){
+        
+    }
+
+    public function setNom($nom){
+        $this->nom = $nom;
+    }
+    public function getNom(){
+        return $this->nom;
+    }
+    public function setPrenom($prenom){
+        $this->prenom = $prenom;
+    }
+    public function setAge($age){
+        $this->age = $age;
+    }
+    public function setGenre($genre){
+        $this->genre = $genre;
+    }
+    public function setIsSmocker($isSmoker){
+        $this->isSmoker = $isSmoker;
+    }
 }
-$unePersonne = new Personne;
-$unePersonne->nom = "DELANNOY";
-$unePersonne->prenom = "Arthur";
-$unePersonne->age = 19;
-$unePersonne->genre = "Masculin";
-$unePersonne->isSmoker = false;
-var_dump($unePersonne->smoker());
+$unePersonne = new Personne("DELANNOY", "Arthur", 19, "Masculin", false);
+$unePersonne = new Personne("DELANNOY", "Arthur", 19, "Masculin");
+#$unePersonne->setNom("DELANNOY") ;
+#$unePersonne->setPrenom("Arthur");
+#$unePersonne->setAge(19);
+#$unePersonne->setGenre("Masculin");
+#$unePersonne->setIsSmocker(false);
+var_dump($unePersonne);
+echo($unePersonne->getNom());
